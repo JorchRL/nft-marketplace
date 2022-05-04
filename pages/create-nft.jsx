dee45ac968fce7b5a58976714d8cc24ebc428ed5
Jorge Romero Lagunas <jorchrl@Jorges-MacBook-Pro.local>
2022-05-04 09:12:07 -0500

    Fix. Import <Head> in create-nft.jsx

--- pages/create-nft.jsx
+++ pages/create-nft.jsx
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 import { create as ipfsHttpClient } from "ipfs-http-client";
 import { useRouter } from "next/Router";
 import Web3Modal from "web3modal";
+import Head from "next/head";
 
 const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
